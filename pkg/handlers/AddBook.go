81041e016c69f700a113dd51a8ddf45bec77a783
fularani <ranivhansure@gmail.com>
2022-09-29 18:19:02 +0530

    insert values into table in postgres with create function

--- pkg/handlers/AddBook.go
+++ pkg/handlers/AddBook.go
@@ -3,8 +3,27 @@ package handlers
 import(
 	"net/http"
 	"github.com/gin-gonic/gin"
+	"project/models"
+	"project/pkg/dbhelpers"
+	"fmt"
 )
 
 func (h handler) AddBook(c *gin.Context) {
-    c.JSON(http.StatusOK, gin.H{"message": "Added a Book successfully",})
+
+	var book=[]models.Book{
+		{ Title:"Think and Grow Rich",Author:"Napoleon Hill",Desc:"It inspire more people to become millionaires and billionaires" },
+		{ Title:"The Alchemist",Author:"Paulo Coelho",Desc:"Paulo Coelho's masterpiece tells the mystical story of Santiago, an Andalusian shepherd boy who yearns to travel in search of a worldly treasure." },
+		{ Title:"Rich Dad, Poor Dad",Author:"Robert T. Kiyosaki",Desc:"Rich Dad Poor Dad is Robert's story of growing up with two dads" },
+	}
+	h.DB.Create(&book)
+
+	var addbook=models.Book{ Title:"How to Win Friends & Influence People",Author:"Dale Carnegie ",Desc:"Dale Carnegie’s rock-solid, time-tested advice has carried countless people up the ladder of success in their business and personal lives." }
+	k := dbhelpers.InsertQuery(h.DB, &addbook)
+	fmt.Println(k.RowsAffected)
+
+	var data_payload []interface{}
+	book = append(book,addbook)
+	data_payload = append(data_payload, book)
+
+    c.JSON(http.StatusOK, gin.H{"data":data_payload,"message": "Added a Book successfully",})
 }
